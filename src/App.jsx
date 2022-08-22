ce7c276ef7b239a183d37f28b1c0514c678452ea
jcarlos2n <eljcarlos2n@gmail.com>
2022-08-22 14:34:08 +0200

    feat: add components styles to Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
+import { BrowserRouter } from 'react-router-dom'
+import Header from "./components/Header/Header"
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect className="headerWall text-white m-0 p-0" expand="md" variant="dark">
+            <Container fluid className="black">
+                <Navbar.Brand as={Link} to="/">LOGO</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" className="py-0 px-0 my-0 mx-3" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/" className="text-white mx-2">Home</Nav.Link>
+                        <Nav.Link as={Link} to="films" className="text-white mx-2">Films</Nav.Link>
+                        <Nav.Link as={Link} to="/aboutus" className="text-white mx-2">About Us</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as={Link} to="/signup" className="text-white mx-2" >Sign Up</Nav.Link>
+                        <Nav.Link as={Link} to="/login" className="text-white mx-2" >Log In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+
+
+}
+
+export default Header;
