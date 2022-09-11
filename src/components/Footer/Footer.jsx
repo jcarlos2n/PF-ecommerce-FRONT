df582c41d03b1b8867cc382c0518c1e38f7fc474
jcarlos2n <eljcarlos2n@gmail.com>
2022-09-11 18:50:48 +0200

    feat: add styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,11 +12,21 @@ const Footer = props => {
                     <a href="https://geekshubsacademy.com/"><img className="logo" src="https://avatars.githubusercontent.com/u/36510045?s=280&v=4" alt="" /></a>
                     <img className="logo" src="https://cdn.icon-icons.com/icons2/2037/PNG/512/facebook_fb_media_social_icon_124262.png" alt="" />
                 </div>
-                <div>
-
+            </div>
+            <div className="footerMiddle">
+                <div className="footLeft">
+                    <a className="linkFoot">Instalaciones</a>
+                    <a className="linkFoot">Contacto</a>
+                    <a className="linkFoot">Preguntas frecuentes</a>
+                    <a className="linkFoot">Noticias</a>
+                </div>
+                <div className="footRight">
+                    <a className="linkFoot">Privacidad</a>
+                    <a className="linkFoot">Aviso legal</a>
+                    <a className="linkFoot">Política de cookies</a>
+                    <a className="linkFoot">Opiniones de clientes</a>
                 </div>
             </div>
-            <div className="footerMiddle"></div>
             <div className="footerBottom"></div>
         </div>
     )
