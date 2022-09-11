5d79eaf0912bdfb32313f52127f5c04998befa41
jcarlos2n <eljcarlos2n@gmail.com>
2022-09-11 18:14:25 +0200

    feat: adding styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,13 +27,13 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" className="py-0 px-0 my-0 mx-3" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/" className="text-white mx-2">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/products" className="text-white mx-2">Products</Nav.Link>
-                            <Nav.Link as={Link} to="/aboutus" className="text-white mx-2">About Us</Nav.Link>
+                            <Nav.Link as={Link} to="/" className="text-white mx-2">Inicio</Nav.Link>
+                            <Nav.Link as={Link} to="/products" className="text-white mx-2">Productos</Nav.Link>
+                            <Nav.Link as={Link} to="/aboutus" className="text-white mx-2">Sobre nosotros</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="/signup" className="text-white mx-2" >Sign Up</Nav.Link>
-                            <Nav.Link as={Link} to="/login" className="text-white mx-2" >Log In</Nav.Link>
+                            <Nav.Link as={Link} to="/signup" className="text-white mx-2" >¿Eres nuevo?</Nav.Link>
+                            <Nav.Link as={Link} to="/login" className="text-white mx-2" >Identificarse</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -48,12 +48,12 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" className="py-0 px-0 my-0 mx-3" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/" className="text-white mx-2">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/products" className="text-white mx-2">Products</Nav.Link>
-                            <Nav.Link as={Link} to="/aboutus" className="text-white mx-2">About Us</Nav.Link>
+                            <Nav.Link as={Link} to="/" className="text-white mx-2">Inicio</Nav.Link>
+                            <Nav.Link as={Link} to="/products" className="text-white mx-2">Productos</Nav.Link>
+                            <Nav.Link as={Link} to="/aboutus" className="text-white mx-2">Sobre nosotros</Nav.Link>
                         </Nav>
                         <Nav>
-                            <NavDropdown title="Cart" className="text-white">
+                            <NavDropdown title="Mi Carrito" className="text-white">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
                                     Another action
