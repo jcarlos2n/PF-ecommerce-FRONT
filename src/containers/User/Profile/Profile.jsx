316885512bd1b9fb81fa83d7c41fd4272ebf8ffc
jcarlos2n <eljcarlos2n@gmail.com>
2022-09-23 18:30:24 +0200

    feat: add get of address endpoint to profile

--- src/containers/User/Profile/Profile.jsx
+++ src/containers/User/Profile/Profile.jsx
@@ -1,7 +1,12 @@
+import axios from "axios";
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {  useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { logout, logOutUser, userData, profileUser, profile } from "../userSlice";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './Profile.scss';
 
 
@@ -10,15 +15,33 @@ const Profile = () => {
     const dispatch = useDispatch();
     let navigate = useNavigate();
     const dataUser = useSelector(userData);
-   
+    const [dataAdd, setAddress] = useState([]);
+
     useEffect(() => {
         if (!dataUser?.token) {
             navigate('/');
-        }else{
+        } else {
             dispatch(profileUser(dataUser.token))
+
+            async function fetchAddress() {
+                try {
+                    const config = {
+                        headers: { "Authorization": `Bearer ${dataUser.token}` }
+                    }
+                    await axios.get('http://localhost:8000/api/user/address/get', config)
+                        .then(resp => {
+                            setAddress(resp.data);
+                        })
+                } catch (error) {
+                    console.log(error)
+                }
+
+
+            }
+            fetchAddress();
         }
-        
-    },[])
+
+    }, [])
     const getOut = () => {
 
         dispatch(logout());
@@ -35,7 +58,19 @@ const Profile = () => {
         navigate('/address')
     }
 
-    return(
+    const AddressList = () => {
+        if (dataAdd.length > 0) {
+            return (
+                <Container fluid>
+                    <Row>
+                        <Col>1 of 1</Col>
+                    </Row>
+                </Container>
+            )
+        }
+    }
+
+    return (
         <div className="profileWall">
             <button type="submit" onClick={getOut}>Log Out</button>
             <button type="submit" onClick={address}>add address</button>
